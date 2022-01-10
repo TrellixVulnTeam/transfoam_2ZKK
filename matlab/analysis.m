d9f2ef71f4053b04a9d276d671c7d6a1557f5095
Sam Park <samp1800@live.com>
2022-01-10 12:47:54 -0500

    some production envelope work

--- matlab/analysis.m
+++ matlab/analysis.m
@@ -11,7 +11,7 @@ changeCobraSolver('gurobi6');
 model = readCbModel('../models/mat/GM_iML1515.mat');
 % model2 = readCbModel('../models/mat/test2.mat');
 
-verifyModel(model, 'massBalance', true)
+% verifyModel(model, 'massBalance', true)
 
 %% add accoa demand
 
@@ -23,6 +23,8 @@ model = changeRxnBounds(model, 'EX_phleth_e', -0, 'l');
 model = changeRxnBounds(model, 'EX_o2_e', -20, 'l');
 model = changeRxnBounds(model, 'EX_glc__D_e', -10, 'l');
 % model = changeRxnBounds(model, 'EX_so4_e', -20, 'l');
+
+% model = changeRxnBounds(model, 'GAPN', -9.094641, 'l');
 
 % model = changeObjective(model,'BIOMASS_Ec_iML1515_core_75p37M');
 % model = changeObjective(model,'DM_phb_c');
@@ -56,7 +58,8 @@ figure(2)
 % wt
 deletions = '';
 lineColor = 'blue';
-targetRxn = char('DM_phb_c');
+targetRxn = char('CS');
+% targetRxn = char('ATPS4rpp');
 biomassRxn = char('BIOMASS_Ec_iML1515_core_75p37M');
 geneDelFlag = false;
 nPts = 20;
@@ -71,12 +74,8 @@ hold on
 % acetate modifications
 deletions = {'PTAr', 'ACKr'};
 lineColor = 'green';
-targetRxn = char('DM_phb_c');
-biomassRxn = char('BIOMASS_Ec_iML1515_core_75p37M');
-geneDelFlag = false;
-nPts = 20;
 
-model = changeRxnBounds(model, 'ACS', 2.5, 'l'); % overexpress acs
+model = changeRxnBounds(model, 'ACS', 0.5, 'l'); % overexpress acs
 
 [biomass_ac, phb_ac, lineHandle] = productionEnvelope(model, deletions, lineColor, targetRxn, biomassRxn, geneDelFlag, nPts);
 
@@ -86,10 +85,6 @@ csvwrite('p_e/phb_ac.csv', phb_ac)
 % tca modifications
 deletions = {'SUCOAS', 'SUCDi'};
 lineColor = 'red';
-targetRxn = char('DM_phb_c');
-biomassRxn = char('BIOMASS_Ec_iML1515_core_75p37M');
-geneDelFlag = false;
-nPts = 20;
 
 model = changeRxnBounds(model, 'ACS', 0, 'l'); % normal acs
 
@@ -99,21 +94,64 @@ csvwrite('p_e/biomass_tca.csv', biomass_tca)
 csvwrite('p_e/phb_tca.csv', phb_tca)
 
 % others
-deletions = {'ACALD', 'LDH_D'};
+deletions = {'AGMHE', 'GMHEPPA', 'GMHEPAT', 'GMHEPK'};
 lineColor = 'magenta';
-targetRxn = char('DM_phb_c');
-biomassRxn = char('BIOMASS_Ec_iML1515_core_75p37M');
-geneDelFlag = false;
-nPts = 20;
+
+% model = changeRxnBounds(model, 'EX_glc__D_e', -1, 'l');
 
 % [biomass_o, phb_o, lineHandle] = productionEnvelope(model, deletions, lineColor, targetRxn, biomassRxn, geneDelFlag, nPts);
- 
+
 % csvwrite('p_e/biomass_o.csv', biomass_o)
 % csvwrite('p_e/phb_o.csv', phb_o)
 
 hold off
 
-xlabel('growth rate (h^-^1)')
-ylabel('phb production (mmol/gDWh)')
-title('phb production envelopes')
-legend('wild type', 'acetate modifications', 'tca modifications')
+xlabel('Growth Rate (h^-^1)')
+ylabel('PHB Production (mmol/gDWh)')
+title('PHB Production Envelopes')
+legend('Wild Type', 'Acetate Modifications', 'TCA Modifications')
+
+
+%% production envelope 2
+
+figure(3)
+
+% wt
+deletions = '';
+lineColor = 'blue';
+targetRxn = char('DM_phb_c');
+biomassRxn = char('BIOMASS_Ec_iML1515_core_75p37M');
+geneDelFlag = false;
+nPts = 20;
+
+% model = changeRxnBounds(model, 'GAPN', 0, 'l');
+% model = changeRxnBounds(model, 'GAPN', 0, 'u');
+
+[biomass_wt, phb_wt, lineHandle] = productionEnvelope(model, deletions, lineColor, targetRxn, biomassRxn, geneDelFlag, nPts);
+
+% csvwrite('p_e/biomass_wt.csv', biomass_wt)
+% csvwrite('p_e/phb_wt.csv', phb_wt)
+
+hold on
+
+% others
+deletions = {'SUCDi'};
+% deletions = {'GLUDy', 'GLUSy'};
+lineColor = 'red';
+% 
+% model = changeRxnBounds(model, 'ACS', 0.5, 'l'); % overexpress acs
+% 
+[biomass_ac, phb_ac, lineHandle] = productionEnvelope(model, deletions, lineColor, targetRxn, biomassRxn, geneDelFlag, nPts);
+
+% csvwrite('p_e/biomass_ac.csv', biomass_ac)
+% csvwrite('p_e/phb_ac.csv', phb_ac)
+
+hold off
+
+%% optgene
+
+targetRxn = char('DM_phb_c');
+substrateRxn = char('EX_glc__D_e');
+generxnList = {'GLUDy', 'GLUSy', 'GAPD'};
+
+[x, population, scores, optGeneSol] = optGene(model, targetRxn, substrateRxn, generxnList);
